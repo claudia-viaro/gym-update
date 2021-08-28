e3742214ab98ef6df14169878a250549fece5b68
Claudia Viaro <78303388+claudia-viaro@users.noreply.github.com>
2021-08-28 15:08:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@
 import setuptools
 from pathlib import Path
 #from setuptools import setup
-setuptools.setup(name='gym_m.update',
+setuptools.setup(name='gym_update',
       version='0.0.1',
       description="A OpenAI Gym Env for continuous control",
       long_description=Path("README.md").read_text(),
       long_description_content_type="text/markdown",
                  author="Claudia Viaro",
                  license="MIT",
-      packages=setuptools.find_packages(include="gym_m.update*"),
+      packages=setuptools.find_packages(include="gym_update*"),
       install_requires=['gym']  # And any other dependencies needed
 )
 
